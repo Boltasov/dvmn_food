2e011ab30d5d03eb84ac12eb389c2b07407bb970
Kilsik <ilsiwow@gmail.com>
2023-09-28 18:52:35 +0300

    Add logout

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from .views import profile_view
 
+
+app_name = 'account'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('profile/', profile_view, name='profile'),
 ]
--- dvmn_food/urls.py
+++ dvmn_food/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from foodplan import views
 
 urlpatterns = [
-    path('account/', include('account.urls')),
+    path('accounts/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
