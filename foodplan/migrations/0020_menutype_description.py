3de05295d724337413cefffd2d3ca352bc37cedd
Kilsik <ilsiwow@gmail.com>
2023-10-01 20:44:02 +0300

    Fix migrations' conflict

--- foodplan/migrations/0020_menutype_description.py
+++ foodplan/migrations/0020_menutype_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('foodplan', '0018_alter_ingredient_options_alter_subscription_end_date_and_more'),
+        ('foodplan', '0019_alter_recommendation_options_and_more'),
     ]
 
     operations = [
